479fa5833575dbb6ea71b4c2c557c00a7f30dd92
Joshua <noll.joshua@outlook.com>
2025-12-26 15:10:49 -0500

    Split script into root and user level

--- install.sh
+++ install.sh
@@ -15,7 +15,7 @@ echo "Like Omarchy, but it's mine."
 echo ""
 
 if [ $USER != "root" ]; then
-  echo "This script must be run as root"
+  echo "This script must be run as root!"
   exit 1
 fi
 
@@ -31,9 +31,9 @@ if [ $NEW_USER_PASSWORD != $PASSWORD_CONFIRMATION ]; then
   exit 1
 fi
 
-# Change to root user home dir and download packages.txt
+# Change to root user home dir and download userscript
 cd $HOME
-curl -L -O https://raw.githubusercontent.com/joshrnoll/myarchy/refs/heads/main/packages.txt
+curl -L -O https://raw.githubusercontent.com/joshrnoll/myarchy/refs/heads/main/userscript.sh
 
 # Update pacman and install necessary packages to run the rest of the script
 pacman -Syu --noconfirm
@@ -51,50 +51,13 @@ useradd -m -p $NEW_USER_PASSWORD -G sudo $NEW_USER
 echo "$NEW_USER ALL=(ALL) NOPASSWD:ALL" > /etc/sudoers
 echo "%sudo ALL=(ALL:ALL) ALL" > /etc/sudoers
 
-
-# Ensure ~/.local/bin is in PATH
-export PATH="$HOME/.local/bin:$PATH"
-
-# Install yay (AUR helper) from source if not present
-if ! command -v yay &> /dev/null; then
-  if [[ -d yay ]]; then
-    rm -rf yay/
-  fi
-  git clone https://aur.archlinux.org/yay.git
-  cd yay
-  makepkg -si --noconfirm
-  cd $HOME
-else
-  echo "Yay already installed... YAY!"
-fi
-
-# Install goose CLI tool from GitHub
-if ! command -v goose &> /dev/null; then
-  curl -fsSL https://github.com/block/goose/releases/download/stable/download_cli.sh | CONFIGURE=false bash
-else
-  echo "Goose is already installed... HONK!"
-fi
-
-# Install all packages from packages.txt
-if [ -f "$HOME/packages.txt" ]; then
-  yay -S --needed --noconfirm $(cat $HOME/packages.txt)
-else
-  echo "No packages.txt found! Exiting..."
-  exit 1
-fi
+# Run userscript as new user
+sudo -u $NEW_USER -i bash ./userscript.sh
 
 # Enable ly display manager on tty2 and disable default getty
 if systemctl status ly@tty2.service; then # TODO: Find a better way to check if ly exists
   systemctl enable ly@tty2.service
   systemctl disable getty@tty2.service
-fi
-
-# Initialize and apply dotfiles using chezmoi
-if [ ! -d "$HOME/.local/share/chezmoi" ]; then
-  sudo -u $NEW_USER chezmoi init https://github.com/joshrnoll/dotfiles.git
-  sudo -u $NEW_USER chezmoi apply
-else
-  echo "Chezmoi not installed. Skipping dotfile installation..."
 fi
 
 echo "Installation complete! Rebooting..."
---
+++ userscript.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+
+# Download list of packages
+cd $HOME
+curl -L -O https://raw.githubusercontent.com/joshrnoll/myarchy/refs/heads/main/packages.txt
+
+# Ensure ~/.local/bin is in PATH
+export PATH="$HOME/.local/bin:$PATH"
+
+# Install yay (AUR helper) from source if not present
+if ! command -v yay &> /dev/null; then
+  if [[ -d yay ]]; then
+    rm -rf yay/
+  fi
+  git clone https://aur.archlinux.org/yay.git
+  cd yay
+  makepkg -si --noconfirm
+  cd $HOME
+else
+  echo "Yay already installed... YAY!"
+fi
+
+# Install goose CLI tool from GitHub
+if ! command -v goose &> /dev/null; then
+  curl -fsSL https://github.com/block/goose/releases/download/stable/download_cli.sh | CONFIGURE=false bash
+else
+  echo "Goose is already installed... HONK!"
+fi
+
+# Install all packages from packages.txt
+if [ -f "$HOME/packages.txt" ]; then
+  yay -S --needed --noconfirm $(cat $HOME/packages.txt)
+else
+  echo "No packages.txt found! Exiting..."
+  exit 1
+fi
+
+# Initialize and apply dotfiles using chezmoi
+if command -v chezmoi &> /dev/null; then
+  if [ ! -d "$HOME/.local/share/chezmoi" ]; then
+    chezmoi init https://github.com/joshrnoll/dotfiles.git
+    chezmoi apply
+  fi
+else
+  echo "Chezmoi not installed. Skipping dotfile installation..."
+fi
