df60ee763d8dd566ae7caf1d34084167f16ac08c
Joshua <noll.joshua@outlook.com>
2026-01-03 14:41:56 -0500

    Add idempotency around user creation

--- install.sh
+++ install.sh
@@ -49,16 +49,37 @@ pacman -Rdd --noconfirm iptables || true # Remove conflicting iptables package, 
 pacman -S --noconfirm iptables-nft # Install iptables-nft to avoid conflicts when installing packages.txt
 pacman -S --needed --noconfirm git base-devel sudo # Install necessary base packages to run the rest of the script
 
-# Create sudo group if not created
+# Create sudo group if it doesn't exist
 if ! getent group sudo &> /dev/null; then
+  echo "No sudo group found. Creating..."
   groupadd sudo 
 fi
 
-# Create desired user account with passwordless sudo privileges
-useradd -m -G sudo $NEW_USER
-echo "$NEW_USER:$NEW_USER_PASSWORD" | chpasswd
-echo "%sudo ALL=(ALL:ALL) ALL" >> /etc/sudoers
-echo "$NEW_USER ALL=(ALL) NOPASSWD:ALL" >> /etc/sudoers
+# Add sudo privileges to sudo group if it's not already configured
+if ! grep -q "%sudo ALL=(ALL:ALL) ALL" /etc/sudoers; then
+  echo "Adding sudo group to sudoers file..."
+  echo "%sudo ALL=(ALL:ALL) ALL" >> /etc/sudoers
+fi
+
+# Create desired user account if it doesn't exist
+if ! id -u $NEW_USER; then
+  echo "User $NEW_USER does not exist. Creating..."
+  useradd -m -G sudo $NEW_USER
+  echo "$NEW_USER:$NEW_USER_PASSWORD" | chpasswd
+  echo "$NEW_USER ALL=(ALL) NOPASSWD:ALL" >> /etc/sudoers
+else
+  # Add the user to the sudo group if they aren't already a member
+  # and configure passwordless sudo if not already configured
+  if ! getent group sudo | grep "$NEW_USER"; then
+    echo "User is not in the sudo group. Adding..."
+    usermod -aG sudo $NEW_USER
+  fi 
+
+  if ! grep -q "$NEW_USER ALL=(ALL) NOPASSWD:ALL" /etc/sudoers; then
+    echo "User does not have passwordless sudo privileges. Adding..."
+    echo "$NEW_USER ALL=(ALL) NOPASSWD:ALL" >> /etc/sudoers
+  fi
+fi
 
 # Download userscript
 curl -L -o /home/$NEW_USER/userscript.sh https://raw.githubusercontent.com/joshrnoll/myarchy/refs/heads/main/userscript.sh
