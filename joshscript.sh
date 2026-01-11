4b439098f4e83f66a00dc774254f041ae8ac9fa7
Josh Noll <noll.joshua@outlook.com>
2026-01-10 19:25:09 -0500

    Extract correct path with borgmatic and add progress flag

--- joshscript.sh
+++ joshscript.sh
@@ -6,4 +6,4 @@ BORG_REPO_PASS=$1
 export BORG_PASSPHRASE=$BORG_REPO_PASS # https://torsion.org/borgmatic/reference/configuration/environment-variables/
 
 cd $HOME
-borgmatic extract --archive latest
+borgmatic extract --archive latest --path home/josh --progress
